b81c1834d83cd7e79d1a822993a88fecc974fa1e
Stefan Dimitrov <stefan.r7.dimitrov@gmail.com>
2020-06-10 20:11:24 +0300

    Updates

---
+++ precache-manifest.82ce7ddd820d2efcc8321bfadf947a3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446cad0e9638813404fc162cf29d607b",
+    "url": "/facedetect/index.html"
+  },
+  {
+    "revision": "6a8b6fdca442f8a2be97",
+    "url": "/facedetect/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3da5a5af2b148cae667b",
+    "url": "/facedetect/static/css/main.03297a7a.chunk.css"
+  },
+  {
+    "revision": "6a8b6fdca442f8a2be97",
+    "url": "/facedetect/static/js/2.865d073b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facedetect/static/js/2.865d073b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da5a5af2b148cae667b",
+    "url": "/facedetect/static/js/main.8fd548ab.chunk.js"
+  },
+  {
+    "revision": "432b402916c31a55e773",
+    "url": "/facedetect/static/js/runtime-main.8e94a9ed.js"
+  },
+  {
+    "revision": "fe8a8c381bef755aeeb192d4ac935a4b",
+    "url": "/facedetect/static/media/Logo.fe8a8c38.jpg"
+  }
+]);
